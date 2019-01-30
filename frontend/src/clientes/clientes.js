b44a25909b89f848347273c80a5127fe82b4c95f
HadleyEduardo <hadleyeduardogarcia@gmail.com>
2019-01-30 12:40:33 -0200

    modificação na tabela de clints

--- frontend/src/clientes/clientes.js
+++ frontend/src/clientes/clientes.js
@@ -1,89 +1,78 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
-import './clientes.css'; 
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './clientes.css';
+import { MDBPagination, MDBPageItem, MDBPageNav, MDBCol, MDBRow, MDBIcon, MDBBtn } from "mdbreact";
 
-class clientes extends Component{
 
-    render(){
-        return(
+class clientes extends Component {
+
+    render() {
+        return (
             <div>
-                
                 <div className="container">
-            <div id="top" class="row">
-    <div class="col-md-3">
-        <h2>Clientes</h2>
-    </div>
- 
-    <div class="col-md-6">
-        <div class="input-group h2">
-            <input name="data[search]" class="form-control" id="search" type="text" placeholder="Pesquisar Itens"></input>
-            <span class="input-group-btn">
-                <button class="btn btn-primary" type="submit">
-                Pesquisar
-                </button>
-            </span>
-        </div>
-    </div>
- 
-    <div class="col-md-3">
-            <Link to='/clientes/novo'>
-            <a href="add.html" class="btn btn-primary pull-right h2">Novo Cliente</a>
-            </Link>
-        
-    </div>
-    <div id="list" class="row">
- 
-    <div class="table-responsive col-md-12">
-        <table class="table table-striped" cellspacing="0" cellpadding="0">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>CPF</th>
-                    <th>CADASTRADO</th>
-                    <th class="actions">Ações</th>
-                 </tr>
-            </thead>
-            <tbody>
- 
-                <tr>
-                    <td>01</td>
-                    <td>Hadley Eduardo Louveira Garcia</td>
-                    <td>009.052.711-90</td>
-                    <td>01/01/2015</td>
-                    <td class="actions">
-                        <a class="btn btn-success btn-xs" href="view.html">Visualizar</a>
-                        <a class="btn btn-warning btn-xs" href="edit.html">Editar</a>
-                        <a class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal">Excluir</a>
-                    </td>
-                </tr>
- 
-            </tbody>
-         </table>
- 
-     </div>
-     <div id="bottom" class="row">
-    <div class="col-md-12">
-         
-    <nav aria-label="Page navigation example">
-  <ul class="pagination pg-blue justify-content-center">
-    <li class="page-item disabled">
-      <a class="page-link" tabindex="-1">Previous</a>
-    </li>
-    <li class="page-item"><a class="page-link">1</a></li>
-    <li class="page-item"><a class="page-link">2</a></li>
-    <li class="page-item"><a class="page-link">3</a></li>
-    <li class="page-item">
-      <a class="page-link">Next</a>
-    </li>
-  </ul>
-</nav>
-    </div>
-</div> 
- </div> 
- 
-   </div> 
-            </div>
+                    <div className="row">
+                        <div className="col-sm-3"></div>
+                        <div className="col-sm-6 col-xs-2 justify-content-center">
+                            <form className="form-inline mt-4 mb-4">
+                                <MDBIcon icon="search" />
+                                <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search" />
+                            </form>
+                        </div>
+
+                        <div className="col-sm-3 col-xs-2">
+                            <Link to='/clientes/novo'>
+                                <button className="btn btn-primary btn-sm">+</button>
+                            </Link>
+                        </div>
+                    </div>
+
+                    <table className="table table-striped" cellspacing="0" cellpadding="0">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>CPF</th>
+                                <th>CADASTRADO</th>
+                                <th className="actions">Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <tr>
+                                <td>01</td>
+                                <td>Hadley Eduardo Louveira Garcia</td>
+                                <td>009.052.711-90</td>
+                                <td>01/01/2015</td>
+                                <td class="actions">
+                                    <button className="btn btn-success btn-sm" >Visualizar</button>
+                                    <button className="btn btn-warning btn-sm" >Editar</button>
+                                    <button className="btn btn-danger btn-sm"  >Excluir</button>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                    <div className='row'>
+                        <div className='col-3'>
+                            <div id="paginacao">
+                                <nav aria-label="Page navigation example">
+                                    <ul class="pagination pg-blue justify-content-center">
+                                        <li class="page-item disabled">
+                                            <a class="page-link" tabindex="-1">Previous</a>
+                                        </li>
+                                        <li class="page-item"><a class="page-link">1</a></li>
+                                        <li class="page-item"><a class="page-link">2</a></li>
+                                        <li class="page-item"><a class="page-link">3</a></li>
+                                        <li class="page-item">
+                                            <a class="page-link">Next</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         )
     }
