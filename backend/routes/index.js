a18e72935cbe22e1e76796ba987fecc849f24b8b
Matheus Daniel <matheusdanielgomess@gmail.com>
2019-01-30 11:06:34 -0300

    adicionado rotas para cadastro de produtos

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import ClienteController from '../controllers/clientes';
+import ProdutoController from './../controllers/produto';
 
 const router = express.Router();
 let ctrlCliente = new ClienteController();
+let ctrlProduto = new ProdutoController();
 
 /* GET index page. */
 router.get('/clientes', ctrlCliente.buscaTodos);
+router.get('/produto', ctrlProduto.buscaTodos);
 
 router.post('/clientes/novo', ctrlCliente.novo);
+router.post('/produto/novo', ctrlCliente.novo);
 
 export default router;
