480317c6ddc37d1e3cece48bf09ded5b54366c87
Rafael Castro <rafael.castro1002@gmail.com>
2019-02-04 21:59:07 +0900

    Resolvido bug de mal identificação de banco de Dados vazio!

---
+++ frontend/src/modais/modalAviso.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import { MDBContainer, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBtn } from 'mdbreact';
+
+class modalAviso extends Component{
+
+    render() {
+        return(
+            <MDBContainer>
+                <MDBModal isOpen={this.props.modal} toggle={this.props.toggle} side centered size='sm'>
+                    <MDBModalHeader style={{ backgroundColor: '#FFBB33', color: 'white', fontWeight: 'bold' }}> <MDBIcon icon="exclamation-triangle" /> Alerta </MDBModalHeader>
+                    <MDBModalBody>
+                        {this.props.mensagem}
+                    </MDBModalBody>
+                    <MDBModalFooter>
+                        <MDBBtn color="warning" size='sm' onClick={this.props.toggle}>Ok, entendi!</MDBBtn>
+                    </MDBModalFooter>
+                </MDBModal>
+            </MDBContainer>
+        )
+    }
+}
+
+export default modalAviso
