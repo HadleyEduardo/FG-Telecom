17bc473c95870ac20d2acba0e09df9836f81b643
Matheus Daniel <matheusdanielgomess@gmail.com>
2019-02-21 19:27:12 -0400

    commitando p evitar conflito dps

--- frontend/src/estoque/estoque.js
+++ frontend/src/estoque/estoque.js
@@ -1,74 +1,66 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom'
-import { MDBBtn, MDBTable, MDBTableBody, MDBTableHead, MDBRow, MDBCol, MDBIcon } from 'mdbreact';
+import { MDBBtn, MDBRow, MDBCol, MDBIcon, MDBDataTable } from 'mdbreact';
 import axios from 'axios';
 import './estoque.css';
 
-const Tabela = (props) => {
+const DatatablePage = (dados) => {
     return (
-        <MDBTable>
-            <MDBTableHead>
-                <tr>
-                    <th>Código de Barras</th>
-                    <th>Nome do Produto</th>
-                    <th>Marca</th>
-                    <th>Modelo</th>
-                </tr>
-            </MDBTableHead>
-            <MDBTableBody id="tabela"></MDBTableBody>
-        </MDBTable>
-    );
-}
-
-const SearchPage = () => {
-    return (
-        <MDBCol sm="5" xs="3">
-            <form className="form-inline mt-1 mb-4">
-                <MDBIcon icon="search" />
-                <input style={{width: '80%'}} className="form-control form-control-sm ml-2" type="text" placeholder="Pesquisar..." aria-label="Search" />
-            </form>
-        </MDBCol>
+        <MDBDataTable
+            striped
+            bordered
+            hover
+            data={dados}
+        />
     );
 }
 
 class estoque extends Component {
-
     componentDidMount() {
         this.requisicao();
     }
 
     getDados(res) {
-        var dados = res;
-        console.log(dados);
+        const dados = {
+            columns: [
+                {
+                    label: 'Código',
+                    field: 'codigo',
+                    sort: 'asc',
+                    width: 150
+                },
+                {
+                    label: 'Nome',
+                    field: 'name',
+                    sort: 'asc',
+                    width: 270
+                },
+                {
+                    label: 'Marca',
+                    field: 'marca',
+                    sort: 'asc',
+                    width: 200
+                },
+                {
+                    label: 'Modelo',
+                    field: 'modelo',
+                    sort: 'asc',
+                    width: 100
+                }
+            ],
+            rows: [
+                res.data
+            ]
+        };
 
-        var tabela = document.getElementById('tabela');
-
-        for (var i = 0; i < dados.length; ++i) {
-            var tupla = document.createElement('tr');
-            tupla.setAttribute('class', 'produto');
-
-            var codigo = document.createElement('td');
-            var nome = document.createElement('td');
-            var marca = document.createElement('td');
-            var modelo = document.createElement('td');
-
-            codigo.innerHTML = dados[i].codigo;
-            nome.innerHTML = dados[i].nome;
-            marca.innerHTML = dados[i].marca;
-            modelo.innerHTML = dados[i].modelo;
-
-            tupla.appendChild(codigo);
-            tupla.appendChild(nome);
-            tupla.appendChild(marca);
-            tupla.appendChild(modelo);
-            tabela.appendChild(tupla);
-        }
+        ReactDOM.render(<DatatablePage dados={dados} />, document.querySelector('div#tabela'));
     }
 
     requisicao() {
         axios.get('http://localhost:3001/estoque/produto')
             .then(response => {
-                this.getDados(response.data)
+                this.getDados(response)
             })
             .catch(error => console.log(error));
     }
@@ -76,14 +68,10 @@ class estoque extends Component {
     render() {
         return (
             <div className="container">
-                <h2>estocao do seu Hadlei</h2>
+                <h2>Estoque</h2>
                 <br />
                 <MDBRow>
-                    <MDBCol sm="3" xs="0"></MDBCol>
-
-                    <SearchPage />
-
-                    <MDBCol sm="2" xs="0"></MDBCol>
+                    <MDBCol sm="10" xs="0"></MDBCol>
 
                     <MDBCol sm="2" xs="1">
                         <Link to='/estoque/novo-item'>
@@ -93,11 +81,12 @@ class estoque extends Component {
                         </Link>
                     </MDBCol>
                 </MDBRow>
-
-                <Tabela />
+                <div id="tabela">
+                    {/* tentei inserir a tabela aqui com ReactDOM */}
+                </div>
             </div>
         )
     }
 }
 
-export default estoque
+export default estoque;
